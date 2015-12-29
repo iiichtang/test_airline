3692cfc18f3efe81a4ca39390ebee10ae82503c1
iiichtang <iiichtang@gmail.com>
2015-12-29 10:30:37 +0800

    modify the database init

--- base_database.py
+++ base_database.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, Integer, String, Date, Time, DateTime, Float, Boo
 from sqlalchemy.dialects.mysql import DOUBLE
 from sqlalchemy import ForeignKey
 import decimal
+from datetime import datetime
 
 Base = declarative_base()
 
@@ -36,33 +37,31 @@ class Customer(Base):
     duty_free_2014 = Column(Integer)
     duty_free_2015 = Column(Integer)
 
-    def __init__(self, id, card_type, birthday, gender, join_date, last_time, times_2013, times_2014, times_2015,
-                 revenue_2013, revenue_2014, revenue_2015, award_2013, award_2014, award_2015, award_mileage_2013,
-                 award_mileage_2014, award_mileage_2015, tour_code_2013, tour_code_2014, tour_code_2015, duty_free_2013,
-                 duty_free_2014, duty_free_2015):
-        self.id = id
-        self.card_type = card_type
-        self.birthday = birthday
-        self.gender = gender
-        self.join_date = join_date
-        self.times_2013 = times_2013
-        self.times_2014 = times_2014
-        self.times_2015 = times_2015
-        self.revenue_2013 = revenue_2013
-        self.revenue_2014 = revenue_2014
-        self.revenue_2015 = revenue_2015
-        self.award_2013 = award_2013
-        self.award_2014 = award_2014
-        self.award_2015 = award_2015
-        self.award_mileage_2013 = award_mileage_2013
-        self.award_mileage_2014 = award_mileage_2014
-        self.award_mileage_2015 = award_mileage_2015
-        self.tour_code_2013 = tour_code_2013
-        self.tour_code_2014 = tour_code_2014
-        self.tour_code_2015 = tour_code_2015
-        self.duty_free_2013 = duty_free_2013
-        self.duty_free_2014 = duty_free_2014
-        self.duty_free_2015 = duty_free_2015
+    def __init__(self, data_list):
+        self.id = data_list[0]
+        self.card_type = data_list[1]
+        self.birthday = data_list[2]
+        self.gender = data_list[3]
+        self.join_date = data_list[4]
+        self.last_time = data_list[5]
+        self.times_2013 = data_list[6]
+        self.times_2014 = data_list[7]
+        self.times_2015 = data_list[8]
+        self.revenue_2013 = data_list[9]
+        self.revenue_2014 = data_list[10]
+        self.revenue_2015 = data_list[11]
+        self.award_2013 = data_list[12]
+        self.award_2014 = data_list[13]
+        self.award_2015 = data_list[14]
+        self.award_mileage_2013 = data_list[15]
+        self.award_mileage_2014 = data_list[16]
+        self.award_mileage_2015 = data_list[17]
+        self.tour_code_2013 = data_list[18]
+        self.tour_code_2014 = data_list[19]
+        self.tour_code_2015 = data_list[20]
+        self.duty_free_2013 = data_list[21]
+        self.duty_free_2014 = data_list[22]
+        self.duty_free_2015 = data_list[23]
 
 
 class Booking(Base):
@@ -81,20 +80,19 @@ class Booking(Base):
     cbncls = Column(String(10, collation='utf8_unicode_ci'))
     rbd = Column(String(10, collation='utf8_unicode_ci'))
 
-    def __init__(self, customer_id, card_type, birthday, gender, bookdt, depdt, carrier,
-                 fltno, depstn, arrstn, cbncls, rbd):
-        self.customer_id = customer_id
-        self.card_type = card_type
-        self.birthday = birthday
-        self.gender = gender
-        self.bookdt = bookdt
-        self.depdt = depdt
-        self.carrier = carrier
-        self.fltno = fltno
-        self.depstn = depstn
-        self.arrstn = arrstn
-        self.cbncls = cbncls
-        self.rbd = rbd
+    def __init__(self, data_list):
+        self.customer_id = data_list[0]
+        self.card_type = data_list[1]
+        self.birthday = data_list[2]
+        self.gender = data_list[3]
+        self.bookdt = data_list[4]
+        self.depdt = data_list[5]
+        self.carrier = data_list[6]
+        self.fltno = data_list[7]
+        self.depstn = data_list[8]
+        self.arrstn = data_list[9]
+        self.cbncls = data_list[10]
+        self.rbd = data_list[11]
 
 
 class TakeFlight(Base):
@@ -118,25 +116,24 @@ class TakeFlight(Base):
     usdyq = Column(DOUBLE)
     mileage = Column(Integer)
 
-    def __init__(self, customer_id, carrier, fltno, depdate, depstn, arrstn, arcd,
-                 tkno, cpno, usdnet, flcl, facl, bkcls, turc, turc2, usdyq, mileage):
-        self.customer_id = customer_id
-        self.carrier = carrier
-        self.fltno = fltno
-        self.depdate = depdate
-        self.depstn = depstn
-        self.arrstn = arrstn
-        self.arcd = arcd
-        self.tkno = tkno
-        self.cpno = cpno
-        self.usdnet = usdnet
-        self.flcl = flcl
-        self.facl = facl
-        self.bkcls = bkcls
-        self.turc = turc
-        self.turc2 = turc2
-        self.usdyq = usdyq
-        self.mileage = mileage
+    def __init__(self, data_list):
+        self.customer_id = data_list[0]
+        self.carrier = data_list[1]
+        self.fltno = data_list[2]
+        self.depdate = data_list[3]
+        self.depstn = data_list[4]
+        self.arrstn = data_list[5]
+        self.arcd = data_list[6]
+        self.tkno = data_list[7]
+        self.cpno = data_list[8]
+        self.usdnet = data_list[9]
+        self.flcl = data_list[10]
+        self.facl = data_list[11]
+        self.bkcls = data_list[12]
+        self.turc = data_list[13]
+        self.turc2 = data_list[14]
+        self.usdyq = data_list[15]
+        self.mileage = data_list[16]
 
 
 class DutyFree(Base):
@@ -153,7 +150,7 @@ class DutyFree(Base):
     rbd = Column(String(10, collation='utf8_unicode_ci'))
     cbncls = Column(String(10, collation='utf8_unicode_ci'))
     proc_flg = Column(String(10, collation='utf8_unicode_ci'))
-    new_date = Column(Date)
+    new_date = Column(DateTime)
     redeem_flg = Column(String(10, collation='utf8_unicode_ci'))
     card_type = Column(String(10, collation='utf8_unicode_ci'))
     redeen_amt = Column(Integer)
@@ -162,29 +159,36 @@ class DutyFree(Base):
     pay_amt_usd = Column(Integer)
     pay_net_usd = Column(Integer)
 
-    def __init__(self, customer_id, po_no, depdate, fltno, depstn, arrstn, gender,
-                 birthday, rbd, cbncls, proc_flg, new_date, redeem_flg, card_type,
-                 redeen_amt, adminbooking_flg, platform, pay_amt_usd, pay_net_usd):
-        self.customer_id = customer_id
-        self.po_no = po_no
-        self.depdate = depdate
-        self.fltno = fltno
-        self.depstn = depstn
-        self.arrstn = arrstn
-        self.gender = gender
-        self.birthday = birthday
-        self.rbd = rbd
-        self.cbncls = cbncls
-        self.proc_flg = proc_flg
-        self.new_date = new_date
-        self.redeem_flg = redeem_flg
-        self.card_type = card_type
-        self.redeen_amt = redeen_amt
-        self.adminbooking_flg = adminbooking_flg
-        self.platform = platform
-        self.pay_amt_usd = pay_amt_usd
-        self.pay_net_usd = pay_net_usd
+    def __init__(self, data_list):
+        self.customer_id = data_list[0]
+        self.po_no = data_list[1]
+        self.depdate = data_list[2]
+        self.fltno = data_list[3]
+        self.depstn = data_list[4]
+        self.arrstn = data_list[5]
+        self.gender = data_list[6]
+        self.birthday = data_list[7]
+        self.rbd = data_list[8]
+        self.cbncls = data_list[9]
+        self.proc_flg = data_list[10]
+        self.new_date = string_to_datetime(data_list[11])
+        self.redeem_flg = data_list[12]
+        self.card_type = data_list[13]
+        self.redeen_amt = data_list[14]
+        self.adminbooking_flg = data_list[15]
+        self.platform = data_list[16]
+        self.pay_amt_usd = data_list[17]
+        self.pay_net_usd = data_list[18]
 
+
+def string_to_datetime(text):
+    print text
+    data = text.split(" ")
+    yymmdd = data[0].split("-")
+    hhmmss = data[1].split(":")
+    print yymmdd
+    print hhmmss
+    return datetime(int(yymmdd[2]), int(yymmdd[0]), int(yymmdd[1]), int(hhmmss[0]), int(hhmmss[1]), int(hhmmss[2]))
 
 if __name__ == "__main__":
     target_database = 'mysql+pymysql://%s:%s@%s:%s/%s' % (DB_USERNAME, DB_PASSWORD, DB_HOSTNAME,
