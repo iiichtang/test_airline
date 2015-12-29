310799cfb439c8ec7a6d0460705b658c07eddb80
iiichtang <iiichtang@gmail.com>
2015-12-29 10:31:09 +0800

    add functions to insert data

---
+++ base_insert.py
@@ -0,0 +1,51 @@
+from base_database import *
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import func
+from datetime import datetime
+from datetime import timedelta
+import csv
+
+if __name__ == "__main__":
+    target_database = 'mysql+pymysql://%s:%s@%s:%s/%s' % (DB_USERNAME, DB_PASSWORD, DB_HOSTNAME,
+                                                          DB_PORT, DB_DATABASE)
+    engine = create_engine(target_database, echo=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    read_ptr = open('booking.csv', 'r')
+    for row in csv.reader(read_ptr):
+        booking_data = Booking(row)
+        session.add(booking_data)
+    session.commit()
+    read_ptr.close()
+
+    read_ptr = open('customer.csv', 'r')
+    for row in csv.reader(read_ptr):
+        customer_data = Customer(row)
+        session.add(customer_data)
+    session.commit()
+    read_ptr.close()
+
+    read_ptr = open('duty_free.csv', 'r')
+    count = 0
+    for row in csv.reader(read_ptr):
+        if count == 0:
+            count += 1
+            continue
+        duty_free_data = DutyFree(row)
+        session.add(duty_free_data)
+    session.commit()
+    read_ptr.close()
+
+    read_ptr = open('take_flight.csv', 'r')
+    row_count = 0
+    for row in csv.reader(read_ptr):
+        take_flight_data = TakeFlight(row)
+        session.add(take_flight_data)
+        row_count += 1
+        if row_count >= 5000:
+            session.commit()
+            row_count = 0
+    read_ptr.close()
+
+    session.close()
