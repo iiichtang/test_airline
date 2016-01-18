12da5f457f49f4bbbd4a972962464890018e9244
iiichtang <iiichtang@gmail.com>
2016-01-18 09:05:03 +0800

    add get all value functions

--- base_flight_gap.py
+++ base_flight_gap.py
@@ -1,5 +1,4 @@
 from base_database import *
-from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy import Column
 from sqlalchemy import Integer, Float
@@ -13,6 +12,7 @@ from sqlalchemy.sql import func
 import time
 from sqlalchemy import *
 from sqlalchemy.orm import *
+import datetime
 from datetime import datetime
 from datetime import date
 from datetime import timedelta
@@ -34,18 +34,11 @@ class flight_aggregator():
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    def restart_engine(self):
-        self.target_database = 'mysql+pymysql://%s:%s@%s:%s/%s' % (DB_USERNAME, DB_PASSWORD, DB_HOSTNAME,
-                                                                   DB_PORT, DB_DATABASE)
-        self.engine = create_engine(target_database, echo=False)
-        self.Session = sessionmaker(bind=engine)
-        self.session = Session()
-
     def get_customer_list(self, filename):
         self.user_list = []
         read_ptr = open(filename, "r")
         for row in csv.reader(read_ptr):
-            print row[0]
+            # print row[0]
             self.user_list.append(row[0])
 
     def get_flight_record(self, customer_id):
@@ -63,7 +56,7 @@ class flight_aggregator():
             _tmp.pop('_sa_instance_state', None)
             response.append(_tmp)
         self.flight_record_list = response
-        print "flight_record_list: " + str(response)
+        # print "flight_record_list: " + str(response)
 
         session.close()
 
@@ -90,16 +83,11 @@ class flight_aggregator():
 
         frequency = (frequency_count_2013 + frequency_count_2014 + frequency_count_2015) / 3
 
-        print "frequency_count_2013", frequency_count_2013
-        print "frequency_count_2014", frequency_count_2014
-        print "frequency_count_2015", frequency_count_2015
-        print "frequency", frequency
-
         session.close()
 
         result = {"frequency_count_2013": frequency_count_2013, "frequency_count_2014": frequency_count_2014,
                   "frequency_count_2015": frequency_count_2015, "frequency": frequency}
-        print result
+        # print result
         return result
 
     def get_travel_gap(self):
@@ -125,7 +113,7 @@ class flight_aggregator():
         count = 0
 
         if len(self.flight_record_list) <= 1:
-            return {"gap": -1}
+            return {"gap_2013": -1, "gap_2014": -1, "gap_2015": -1, "gap": -1}
 
         for data in self.flight_record_list:
             if data['depdate'] < date(2014, 1, 1):
@@ -161,37 +149,13 @@ class flight_aggregator():
                 end_time = data['depdate']
                 count += 1
 
-        gap_2013 = 0 if count_2013 == 0 else total_time_2013 / count_2013
-        gap_2014 = 0 if count_2014 == 0 else total_time_2014 / count_2014
-        gap_2015 = 0 if count_2015 == 0 else total_time_2015 / count_2015
-        gap = 0 if count == 0 else total_time / count
+        gap_2013 = -1 if count_2013 <= 1 else (total_time_2013 / count_2013).total_seconds()
+        gap_2014 = -1 if count_2014 <= 1 else (total_time_2014 / count_2014).total_seconds()
+        gap_2015 = -1 if count_2015 <= 1 else (total_time_2015 / count_2015).total_seconds()
+        gap = -1 if count <= 1 else (total_time / count).total_seconds()
 
-        print "start_time_2013", start_time_2013
-        print "end_time_2013", end_time_2013
-        print "total_time_2013", total_time_2013
-        print "count_2013", count_2013
-        print "gap_2013", gap_2013
-
-        print "start_time_2014", start_time_2014
-        print "end_time_2014", end_time_2014
-        print "total_time_2014", total_time_2014
-        print "count_2014", count_2014
-        print "gap_2014", gap_2014
-
-        print "start_time_2015", start_time_2015
-        print "end_time_2015", end_time_2015
-        print "total_time_2015", total_time_2015
-        print "count_2015", count_2015
-        print "gap_2015", gap_2015
-
-        print "start_time", start_time
-        print "end_time", end_time
-        print "total_time", total_time
-        print "count", count
-        print "gap", gap
-
-        result = {"gap_2013": gap_2013, "gap_2014": gap_2014, "gap_2015": gap_2015, "gap": gap}
-        print result
+        result = {"gap_2013": int(gap_2013), "gap_2014": int(gap_2014), "gap_2015": int(gap_2015), "gap": int(gap)}
+        # print result
         return result
 
     def get_booking_count(self, customer_id):
@@ -202,7 +166,7 @@ class flight_aggregator():
             .filter(Booking.customer_id == customer_id) \
             .count()
 
-        print "booking_count", booking_count
+        # print "booking_count", booking_count
 
         session.close()
         return booking_count
@@ -217,6 +181,8 @@ class flight_aggregator():
             .all()
 
         revenue_count_2013 = revenue_count_2013[0].result
+        if revenue_count_2013 is None:
+            revenue_count_2013 = 0
 
         revenue_count_2014 = self.session.query(func.sum(TakeFlight.usdnet).label("result")) \
             .filter(TakeFlight.customer_id == customer_id) \
@@ -225,6 +191,8 @@ class flight_aggregator():
             .all()
 
         revenue_count_2014 = revenue_count_2014[0].result
+        if revenue_count_2014 is None:
+            revenue_count_2014 = 0
 
         revenue_count_2015 = self.session.query(func.sum(TakeFlight.usdnet).label("result")) \
             .filter(TakeFlight.customer_id == customer_id) \
@@ -232,12 +200,14 @@ class flight_aggregator():
             .all()
 
         revenue_count_2015 = revenue_count_2015[0].result
+        if revenue_count_2015 is None:
+            revenue_count_2015 = 0
 
         revenue_count = revenue_count_2013 + revenue_count_2014 + revenue_count_2015
 
         result = {"revenue_count_2013": revenue_count_2013, "revenue_count_2014": revenue_count_2014,
                   "revenue_count_2015": revenue_count_2015, "revenue_count": revenue_count}
-        print result
+        # print result
         session.close()
         return result
 
@@ -319,9 +289,176 @@ class flight_aggregator():
 
         result = {"duty_free_count_2013": duty_free_count_2013, "duty_free_count_2014": duty_free_count_2014,
                   "duty_free_count_2015": duty_free_count_2015, "duty_free_count": duty_free_count}
-        print result
+        # print result
         session.close()
         return result
+
+    def get_one_value(self, customer_id):
+        travel_value = 0
+        duty_free_value = 0
+
+        self.get_flight_record(customer_id)
+        travel_gap = self.get_travel_gap()
+        booking_count = self.get_booking_count(customer_id)
+        frequency = self.get_travel_frequency(customer_id)
+        revenue = self.get_revenue(customer_id)
+        duty_free = self.get_duty_free(customer_id, "all")
+
+        print "id", customer_id
+        print "travel_gap", travel_gap
+        print "booking_count", booking_count
+        print "frequency", frequency
+        print "revenue", revenue
+        print "duty_free", duty_free
+
+        result = {"travel_value": travel_value, "duty_free_value": duty_free_value}
+        # print result
+        return result
+
+    def get_all_value(self):
+        self.get_customer_list("customer_id_has_revenue.csv")
+        write_ptr = open("has_revenue_value.csv", 'w')
+        count = 0
+
+        for customer in self.user_list:
+            customer_value = self.get_one_value(customer)
+
+            result = "%s,%s,%s\n" % (customer, customer_value['travel_value'], customer_value['duty_free_value'])
+            write_ptr.write(result)
+
+            count += 1
+            if count == 5:
+                write_ptr.close()
+                return None
+
+        write_ptr.close()
+
+    def get_all_value_from_csv(self, read_filename, write_filename):
+        read_ptr = open(read_filename, 'r')
+        write_ptr = open(write_filename, 'w')
+        count = 0
+
+        for row in csv.reader(read_ptr):
+
+            if count == 0:
+                count += 1
+                continue
+
+            print row
+
+            customer_id = row[0]
+            travel_gap_2013 = int(row[1])
+            travel_gap_2014 = int(row[2])
+            travel_gap_2015 = int(row[3])
+            travel_gap = int(row[4])
+            booking_count = int(row[5])
+            frequency_count_2013 = int(row[6])
+            frequency_count_2014 = int(row[7])
+            frequency_count_2015 = int(row[8])
+            frequency_count = int(row[9])
+            revenue_count_2013 = float(row[10])
+            revenue_count_2014 = float(row[11])
+            revenue_count_2015 = float(row[12])
+            revenue_count = float(row[13])
+            duty_free_count_2013 = float(row[14])
+            duty_free_count_2014 = float(row[15])
+            duty_free_count_2015 = float(row[16])
+            duty_free_count = float(row[17])
+
+            travel_value = 0
+            revenue_value = 0
+            duty_free_value = 0
+            realized_duty_free = 0
+            future_duty_free = 0
+
+            gap_2013 = 0 if travel_gap_2013 <= 1 else 86400 / travel_gap_2013 * 0.2
+            gap_2014 = 0 if travel_gap_2014 <= 1 else 86400 / travel_gap_2014 * 0.3
+            gap_2015 = 0 if travel_gap_2015 <= 1 else 86400 / travel_gap_2015 * 0.5
+            gap_value = (gap_2013 + gap_2014 + gap_2015) * 0.2
+            booking_value = (booking_count / 0.8) * 0.5
+            frequency_value = (
+                                  frequency_count_2013 / 1.5 * 0.2 + frequency_count_2014 / 1.5 * 0.3 + frequency_count_2015 / 1.5 * 0.5) * 0.3
+
+            travel_value = gap_value + booking_value + frequency_value
+
+            realized_revenue = (
+                                   revenue_count_2013 / 2000 * 0.2 + revenue_count_2014 / 2000 * 0.3 + revenue_count_2015 / 2000 * 0.5) * 0.55
+
+            revenue_2013 = 0 if frequency_count_2013 <= 0 else revenue_count_2013 / frequency_count_2013 * 0.2
+            revenue_2014 = 0 if frequency_count_2014 <= 0 else revenue_count_2014 / frequency_count_2014 * 0.3
+            revenue_2015 = 0 if frequency_count_2015 <= 0 else revenue_count_2015 / frequency_count_2015 * 0.5
+            future_revenue = ((booking_count * (revenue_2013 + revenue_2014 + revenue_2015)) / 300) * 0.45
+
+            revenue_value = realized_revenue + future_revenue
+
+            realized_duty_free = (
+                                     duty_free_count_2013 / 200 * 0.2 + duty_free_count_2014 / 200 * 0.3 + duty_free_count_2015 / 200 * 0.5) * 0.55
+
+            duty_free_2013 = 0 if frequency_count_2013 <= 0 else duty_free_count_2013 / frequency_count_2013 * 0.2
+            duty_free_2014 = 0 if frequency_count_2014 <= 0 else duty_free_count_2014 / frequency_count_2014 * 0.3
+            duty_free_2015 = 0 if frequency_count_2015 <= 0 else duty_free_count_2015 / frequency_count_2015 * 0.5
+            future_duty_free = ((booking_count * (duty_free_2013 + duty_free_2014 + duty_free_2015)) / 50) * 0.45
+
+            duty_free_value = realized_duty_free + future_duty_free
+
+            result = "%s,%s,%s,%s\n" % (customer_id, travel_value, revenue_value, duty_free_value)
+            write_ptr.write(result)
+
+        read_ptr.close()
+        write_ptr.close()
+
+        """
+        0  id,
+        1  travel_gap_2013,
+        2  travel_gap_2014,
+        3  travel_gap_2015,
+        4  travel_gap,
+        5  booking_count
+        6  frequency_count_2013,
+        7  frequency_count_2014,
+        8  frequency_count_2015,
+        9  frequency_count
+        10 revenue_count_2013,
+        11 revenue_count_2014,
+        12 revenue_count_2015,
+        13 revenue_count
+        14 duty_free_count_2013,
+        15 duty_free_count_2014,
+        16 duty_free_count_2015,
+        17 duty_free_count
+        """
+
+    def get_all_result(self, read_filename, write_filename):
+        self.get_customer_list(read_filename)
+
+        write_prt = open(write_filename, 'wb')
+
+        result = "id,travel_gap_2013,travel_gap_2014,travel_gap_2015,travel_gap,booking_count," \
+                 "frequency_count_2013,frequency_count_2014,frequency_count_2015,frequency_count," \
+                 "revenue_count_2013,revenue_count_2014,revenue_count_2015,revenue_count," \
+                 "duty_free_count_2013,duty_free_count_2014,duty_free_count_2015,duty_free_count\n"
+        print result
+        write_prt.write(result)
+
+        for data in self.user_list:
+            self.get_flight_record(data)
+            travel_gap = self.get_travel_gap()
+            booking_count = self.get_booking_count(data)
+            frequency = self.get_travel_frequency(data)
+            revenue = self.get_revenue(data)
+            duty_free = self.get_duty_free(data, "all")
+
+            result = "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (
+                data, travel_gap['gap_2013'], travel_gap['gap_2014'], travel_gap['gap_2015'], travel_gap['gap'],
+                booking_count, frequency['frequency_count_2013'], frequency['frequency_count_2014'],
+                frequency['frequency_count_2015'], frequency['frequency'], revenue['revenue_count_2013'],
+                revenue['revenue_count_2014'], revenue['revenue_count_2015'], revenue['revenue_count'],
+                duty_free['duty_free_count_2013'], duty_free['duty_free_count_2014'], duty_free['duty_free_count_2015'],
+                duty_free['duty_free_count'])
+            print result
+            write_prt.write(result)
+
+        write_prt.close()
 
 
 if __name__ == "__main__":
@@ -334,4 +471,17 @@ if __name__ == "__main__":
     # flight_aggregate.get_revenue("19920400000403")
     # flight_aggregate.get_duty_free("19890300000711", "pay_amt_usd")
     # flight_aggregate.get_duty_free("19890300000711", "pay_net_usd")
-    flight_aggregate.get_duty_free("19890300000711", "all")
+    # flight_aggregate.get_duty_free("19890300000711", "all")
+    # flight_aggregate.get_one_value("19920400000403")
+    # flight_aggregate.get_one_value("19890100000210")
+
+    # flight_aggregate.get_all_result("customer_id_has_revenue.csv", "has_revenue_metadata.csv")
+    # flight_aggregate.get_all_value()
+    # flight_aggregate.get_all_value_from_csv("has_revenue_metadata.csv", "has_revenue_value.csv")
+
+
+    for title_count in range(2, 8):
+        print title_count
+        read_filename = "customer_%s.csv" % title_count
+        write_filename = "all_value_%s.csv" % title_count
+        flight_aggregate.get_all_result(read_filename, write_filename)
--- test.py
+++ test.py
@@ -1,9 +1,37 @@
 import csv
 
-
+"""
 read_ptr = open('take_flight.csv.txt', 'r')
 count = 0
 for row in csv.reader(read_ptr):
     if row[10] is "" or row[15] is "":
         count += 1
 print count
+
+"""
+
+title_count = 0
+
+read_ptr = open('customer_all.csv', 'r')
+
+
+filename = "customer_%s.csv" % title_count
+
+write_ptr = open(filename, 'w')
+
+count = 0
+
+for row in csv.reader(read_ptr):
+    result = "%s" % row[0]
+    write_ptr.write(result)
+    count += 1
+    if count > 1000:
+        write_ptr.close()
+        title_count += 1
+        filename = "customer_%s.csv" % title_count
+        write_ptr = open(filename, 'w')
+        count = 0
+        pass
+    else:
+        result = "\n"
+        write_ptr.write(result)
